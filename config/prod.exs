b41f76be975faf4d255ad0a5d8e972df1924356b
bartekupartek <ebartlomiej@gmail.com>
2021-03-15 14:40:54 +0100

    Revert "configurable host"
    This reverts commit 2ec3de1cc66ec36bbebae0f86124c3ddcce686e0.

--- config/prod.exs
+++ config/prod.exs
@@ -17,9 +17,3 @@ config :imager, ImagerWeb.Endpoint, server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-config :vmstats,
-  sink: Imager.Stats,
-  base_key: "erlang"
-
-config :statix, host: "${STATS_HOST}"
